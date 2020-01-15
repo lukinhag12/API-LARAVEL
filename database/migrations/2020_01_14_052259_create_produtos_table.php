2b493e42c3689e284a9eb91c9d9b5e720ce3d4aa
Lucas TI <lukinha.g12.g12@gmail.com>
2020-01-15 03:14:28 -0300

    Add files via upload

---
+++ database/migrations/2020_01_14_052259_create_produtos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('descricao');
+            $table->float('oreco');
+            $table->integer('quantidade');
+            $table->string('marca');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
